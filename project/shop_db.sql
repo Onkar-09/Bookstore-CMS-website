8efab3c099fd63e2dab69c20bb0de71868a2e53e
Onkar-09 <157623816+Onkar-09@users.noreply.github.com>
2025-03-11 16:11:27 +0530

    Add files via upload

--- project/shop_db.sql
+++ project/shop_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 03, 2022 at 01:54 PM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 8.1.2
+-- Generation Time: Mar 11, 2025 at 11:40 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -34,7 +34,7 @@ CREATE TABLE `cart` (
   `price` int(100) NOT NULL,
   `quantity` int(100) NOT NULL,
   `image` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -49,7 +49,14 @@ CREATE TABLE `message` (
   `email` varchar(100) NOT NULL,
   `number` varchar(12) NOT NULL,
   `message` varchar(500) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `message`
+--
+
+INSERT INTO `message` (`id`, `user_id`, `name`, `email`, `number`, `message`) VALUES
+(10, 1, 'Onkar Bhute', 'dunudbhur@gmail.com', '2435678', 'fteuvdb ufy gwdp hi ');
 
 -- --------------------------------------------------------
 
@@ -69,7 +76,15 @@ CREATE TABLE `orders` (
   `total_price` int(100) NOT NULL,
   `placed_on` varchar(50) NOT NULL,
   `payment_status` varchar(20) NOT NULL DEFAULT 'pending'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `user_id`, `name`, `number`, `email`, `method`, `address`, `total_products`, `total_price`, `placed_on`, `payment_status`) VALUES
+(10, 4, 'new', '1234567', 'new@gmail.com', 'paypal', 'flat no. 161, bknlm, mu, in - 1234564', ', bash_and_lucy-2 (1) , holy_ghosts (1) , lloyd (4) ', 1010, '07-Mar-2025', 'pending'),
+(11, 1, 'Omkar Bhute', '2345', 'onkarbhute99@gmail.com', 'credit card', 'flat no. 1, scvb, Nagpur, India - 440015', ', bash_and_lucy-2 (1) , holy ghost (2) ', 570, '08-Mar-2025', 'completed');
 
 -- --------------------------------------------------------
 
@@ -82,7 +97,20 @@ CREATE TABLE `products` (
   `name` varchar(100) NOT NULL,
   `price` int(100) NOT NULL,
   `image` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `price`, `image`) VALUES
+(6, 'bash_and_lucy-2', 290, 'bash_and_lucy-2.jpg'),
+(7, 'holy ghost', 140, 'holy_ghosts.jpg'),
+(8, 'free fall', 185, 'freefall.jpg'),
+(9, 'ancient 2', 562, 'history_of_modern_architecture.jpg'),
+(10, 'darknet', 100, 'darknet.jpg'),
+(11, 'economics', 125, 'economic.jpg'),
+(12, 'be well be', 400, 'be_well_bee.jpg');
 
 -- --------------------------------------------------------
 
@@ -96,7 +124,17 @@ CREATE TABLE `users` (
   `email` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
   `user_type` varchar(20) NOT NULL DEFAULT 'user'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `user_type`) VALUES
+(1, 'Onkar Bhute', 'dunudbhur@gmail.com', '62c8ad0a15d9d1ca38d5dee762a16e01', 'user'),
+(3, 'oppb', 'oppb@gmail.com', '350c9d901b7f1c73a481e240ec1b7c0b', 'user'),
+(4, 'new', 'new@gmail.com', '68a0099b3f45357798639a30c5fe3154', 'user'),
+(6, 'admin', 'admin@gmail.com', '0192023a7bbd73250516f069df18b500', 'admin');
 
 --
 -- Indexes for dumped tables
@@ -140,31 +178,31 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=62;
 
 --
 -- AUTO_INCREMENT for table `message`
 --
 ALTER TABLE `message`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
